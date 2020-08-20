b75f4580d783fcf534732203c0a6f2c7cca947d2
Kaushalya Kularatnam <kk2219@login-6.rcs.ic.ac.uk>
2020-08-20 13:16:25 +0100

    Further Changes

---
+++ src/train.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def initialize_weights(shape, name=None, dtype=None):
+    """
+        The paper, http://www.cs.utoronto.ca/~gkoch/files/msc-thesis.pdf
+        suggests to initialize CNN layer weights with mean as 0.0 and standard deviation of 0.01
+    """
+    return np.random.normal(loc = 0.0, scale = 1e-2, size = shape)
+
+def initialize_bias(shape, name=None, dtype=None):
+    """
+        The paper, http://www.cs.utoronto.ca/~gkoch/files/msc-thesis.pdf
+        suggests to initialize CNN layer bias with mean as 0.5 and standard deviation of 0.01
+    """
+    return np.random.normal(loc = 0.5, scale = 1e-2, size = shape)
