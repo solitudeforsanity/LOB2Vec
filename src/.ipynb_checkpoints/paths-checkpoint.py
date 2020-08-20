357f91c4d26d65a1650cd1e782dd8d8141afd670
Kaushalya Kularatnam <kk2219@login-5.rcs.ic.ac.uk>
2020-08-20 11:49:49 +0100

    Removing redundant files

--- src/.ipynb_checkpoints/paths-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import os
-
-source_2016 = os.path.abspath(os.path.join('../../data/full_depth/2016'))
-source_2017 = os.path.abspath(os.path.join('../../data/full_depth/2017'))
-dest_2016 = os.path.abspath(os.path.join('../../data/LOB2Vec/Production/2016'))
-dest_2017 = os.path.abspath(os.path.join('../../data/LOB2Vec/Production/2017'))
-test_source = os.path.abspath(os.path.join('../../data/LOB2Vec/Research/source'))
-test_dest = os.path.abspath(os.path.join('../../data/LOB2Vec/Research/destination'))
